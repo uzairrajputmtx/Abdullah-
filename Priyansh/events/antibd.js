37c7db03eb127cc0b00b15ec5cbc30794ccb0778
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-04 22:01:47 +0500

    Create antibd.js

--- Priyansh/events/antibd.js
+++ Priyansh/events/antibd.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "antibd",
   eventType: ["log:user-nickname"],
   version: "0.0.1",
-  credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+  credits: "uzairrajput",
   description: "Against changing Bot's nickname"
 };
 
