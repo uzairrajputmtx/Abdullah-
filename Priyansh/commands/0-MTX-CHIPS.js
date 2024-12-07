fc2a025f0fd24aaa8bb32a9dfab57b6c5f4fa6ed
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-07 21:27:18 +0500

    Create  0-MTX-CHIPS.js

--- Priyansh/commands/0-MTX-CHIPS.js
+++ Priyansh/commands/0-MTX-CHIPS.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "chips",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "AADI BABU", 
+        credits: "uzairrajput", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "Chips",
@@ -14,8 +14,8 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("chips")==0 || event.body.indexOf("Chips")==0 || event.body.indexOf("CHIPS ")==0 || event.body.indexOf("Chipas")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝗦𝗛𝗔𝗔𝗡 𝗞𝗛𝗔𝗡 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐜𝐇𝐚𝐔𝐦𝐈𝐧 \n__________________________________ ",
-                                attachment: fs.createReadStream(__dirname + `/noprefix/Pinterest-mini-aesthetics-snack-party.mp4`)
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕  \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐜𝐇𝐚𝐔𝐦𝐈𝐧 \n__________________________________ ",
+                                attachment: fs.createReadStream(__dirname + `/mtxuzair/Pinterest-mini-aesthetics-snack-party.mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🥟", event.messageID, (err) => {}, true)
