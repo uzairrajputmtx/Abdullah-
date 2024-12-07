c681a253201d0681d49b193567af71602a85486f
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-07 22:11:06 +0500

    Create 0_MTX-JUICES.js

--- Priyansh/commands/0_MTX-JUICES.js
+++ Priyansh/commands/0_MTX-JUICES.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "juices",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "AADI BABU", 
+        credits: "uzairrajput", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "juice",
@@ -14,8 +14,8 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("juice")==0 || event.body.indexOf("juices")==0 || event.body.indexOf("Juice")==0 || event.body.indexOf("JUICE")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐉𝐮𝐢𝐂𝐞\n__________________________________ ",
-                                attachment: fs.createReadStream(__dirname + `/noprefix/Never_forget_to_hydrate_your_body_every_day🥤🍓😋_|_Gıda,_Smoothies,_Çilekler(480P).mp4`)
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻ 𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕   \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐉𝐮𝐢𝐂𝐞\n__________________________________ ",
+                                attachment: fs.createReadStream(__dirname + `/mtxuzair/Never_forget_to_hydrate_your_body_every_day🥤🍓😋_|_Gıda,_Smoothies,_Çilekler(480P).mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🍺", event.messageID, (err) => {}, true)
