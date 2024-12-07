a4324ee9461816de8f9f1b66c2bf4fda52c21f2b
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-07 22:38:26 +0500

    Create  0_MTX-PURPOSE.js

--- Priyansh/commands/0_MTX-PURPOSE.js
+++ Priyansh/commands/0_MTX-PURPOSE.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "purpose",
   version: "7.3.1",
   hasPermssion: 0,
-  credits: " Chand",///don't change my Credit Coz i Edit 
+  credits: " uzairrajput",///don't change my Credit uzairrajput edit
   description: "Get Pair From Mention",
   commandCategory: "img",
   usages: "[@mention]",
