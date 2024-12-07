d56af4b76305ea04c21a0ae3bbf4f29174c8ad71
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-07 21:28:08 +0500

    Create 0_AADI-COFFEE.js

--- Priyansh/commands/0_AADI-COFFEE.js
+++ Priyansh/commands/0_AADI-COFFEE.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "Coffee",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "AADI BABU", 
+        credits: "uzairrajput", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "Coffee",
@@ -14,8 +14,8 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("Coffee")==0 || event.body.indexOf("coffee")==0 || event.body.indexOf("COFFEE ")==0 || event.body.indexOf("cofi")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝗦𝗛𝗔𝗔𝗡 𝗞𝗛𝗔𝗡 𝗞 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐎𝐅𝐅𝐄𝐄 \n__________________________________ ",
-                                attachment: fs.createReadStream(__dirname + `/noprefix/Pinterest-boomerang.mp4`)
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕  \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐎𝐅𝐅𝐄𝐄 \n__________________________________ ",
+                                attachment: fs.createReadStream(__dirname + `/mtxuzair/Pinterest-boomerang.mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("☕", event.messageID, (err) => {}, true)
