34baa900ec91f0da5c3227b75aa658ab61c81e6f
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-08 03:03:44 +0500

    Create Obama.js

--- Priyansh/commands/Obama.js
+++ Priyansh/commands/Obama.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "obama",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "uzairrajput",
 	description: "Obama Tweet post",
 	commandCategory: "edit-img",
 	usages: "[text]",
