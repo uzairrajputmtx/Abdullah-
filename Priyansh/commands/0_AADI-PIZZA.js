19547b6d2ffdb5abd08310a70d3a743072822ed3
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-07 22:31:38 +0500

    Create 0_AADI-PIZZA.js

--- Priyansh/commands/0_AADI-PIZZA.js
+++ Priyansh/commands/0_AADI-PIZZA.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "pizza",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "AADI BABU", 
+        credits: "uzairrajput", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "pizza",
@@ -14,8 +14,8 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("pizza")==0 || event.body.indexOf("Pizza")==0 || event.body.indexOf("PIZZA")==0 || event.body.indexOf("PIZZAA")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐡𝐚𝐚𝐧 𝐊𝐡𝐚𝐧 𝐊 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐈𝐙𝐙𝐀 \n__________________________________ ",
-                                attachment: fs.createReadStream(__dirname + `/noprefix/Pinterest-Abhijeet-Agnihotri-pizza.mp4`)
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕  \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐈𝐙𝐙𝐀 \n__________________________________ ",
+                                attachment: fs.createReadStream(__dirname + `/mtxuzair/Pinterest-Abhijeet-Agnihotri-pizza.mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🍕", event.messageID, (err) => {}, true)
