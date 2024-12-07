e5f8f1dd41067c8c916d69a1ebb10f6c69059113
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-07 21:23:50 +0500

    Create 0_MTX-CHAUMIN.js

--- Priyansh/commands/0_MTX-CHAUMIN.js
+++ Priyansh/commands/0_MTX-CHAUMIN.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "chaumin",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "AADI BABU", 
+        credits: "uzairrajput", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "chaumin",
@@ -14,8 +14,8 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("chaumin")==0 || event.body.indexOf("Chaumin")==0 || event.body.indexOf("CHAUMIN")==0 || event.body.indexOf("chauminn")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝑺𝑯𝑨𝑨𝑵 𝑲𝑯𝑨𝑵 𝑲 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐜𝐇𝐚𝐔𝐦𝐈𝐧 \n__________________________________ ",
-                                attachment: fs.createReadStream(__dirname + `/noprefix/8a9f84ed741b6df2854fb136dd028e79.jpg`)
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕  \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐜𝐇𝐚𝐔𝐦𝐈𝐧 \n__________________________________ ",
+                                attachment: fs.createReadStream(__dirname + `/mtxuzair/8a9f84ed741b6df2854fb136dd028e79.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🍜", event.messageID, (err) => {}, true)
