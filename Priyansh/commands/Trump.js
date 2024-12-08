c0aac5fe238045c086c983d559c53524456ebff1
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-08 22:21:25 +0500

    Create Trump.js

--- Priyansh/commands/Trump.js
+++ Priyansh/commands/Trump.js
@@ -4,7 +4,7 @@ module.exports.config = {
 
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "uzairrajput",
 	description: "Comment on the board ( ͡° ͜ʖ ͡°)",
 	commandCategory: "edit-img",
 	usages: "trump [text]",
