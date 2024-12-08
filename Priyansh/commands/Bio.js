c61954f598ffb69d8a90169f9c3cff907c70a588
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-08 21:58:12 +0500

    Create Bio.js

--- Priyansh/commands/Bio.js
+++ Priyansh/commands/Bio.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "bio",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "uzairrajput",
 	description: "Change bot's bio",
 	commandCategory: "admin",
 	usages: "bio [text]",
