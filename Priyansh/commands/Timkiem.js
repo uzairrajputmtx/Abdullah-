17bf13e38864c7ff6fc1fcaffede2cd54c296642
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-08 22:20:10 +0500

    Create Timkiem.js

--- Priyansh/commands/Timkiem.js
+++ Priyansh/commands/Timkiem.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "search",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "uzairrajput",
     description: "Search results on google",
     commandCategory: "info",
     usages: "search [Text]",
