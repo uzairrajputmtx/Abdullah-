b691f852292f6dd658ca49736d02d469b84a1240
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-09 02:35:55 +0500

    Create Mtxai.js

---
+++ Priyansh/commands/Mtxai.js
@@ -0,0 +1,38 @@
+module.exports.config = {
+  name: "ai",
+  version: "1.2.1",
+  hasPermssion: 0,
+  credits: "uzairrajput ",
+  description: "( 𝘼𝙄 )",
+  commandCategory: "utilities",
+  usages: "Ask ( random )",
+  cooldowns: 3,
+  dependencies: {}
+};
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+module.exports.run = async function({ api, event, args }) {
+  let message = args.join(" ");
+
+  if (message.length < 1) {
+    api.sendMessage("𝖨𝗇𝗏𝖺𝗅𝗂𝖽 𝖢𝗈𝗆𝗆𝖺𝗇𝖽\n 𝖴𝗌𝖺𝗀𝖾: ai <𝖺𝗌𝗄> )\nCreated by : 𝑴𝑻𝑿 💚✨", event.threadID);
+  } else {
+    try {
+      const response = await axios.get(`https://duckduckgo.com/html/?q=${encodeURIComponent(message)}`);
+      const $ = cheerio.load(response.data);
+      const answer = $(".result__snippet").first().text();
+
+      if (answer) {
+        api.sendMessage(answer, event.threadID, event.messageID);
+      } else {
+        api.sendMessage("𝖲𝗈𝗋𝗋𝗒 𝖨 𝖼𝗈𝗎𝗅𝖽𝗇'𝗍 𝖿𝗂𝗇𝖽 𝗍𝗁𝖾 𝖺𝗇𝗌𝗐𝖾𝗋..")
+            (event.threadID);
+       }
+    } catch (error) {
+      console.error(error);
+      api.sendMessage("‼️𝖤𝗋𝗋𝗈𝗋 𝗐𝗁𝗂𝗅𝖾 𝖿𝖾𝗍𝖼𝗁𝗂𝗇𝗀 𝗍𝗁𝖾 𝖺𝗇𝗌𝗐𝖾𝗋..", event.threadID);
+    }
+  }
+};
