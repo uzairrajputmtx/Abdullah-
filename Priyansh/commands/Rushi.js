1460b4980779493931281a745dc5e255c4d22ff7
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-08 22:19:09 +0500

    Create Rushi.js

--- Priyansh/commands/Rushi.js
+++ Priyansh/commands/Rushi.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "rushia",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+ credits: "uzairrajput",
  description: "Random Rushia",
  commandCategory: "random-img",
  usages: "rushia",
