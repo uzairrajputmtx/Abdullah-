8bb54b0d29c52040894fca050326c9e63d5b246a
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-10 00:26:38 +0500

    Create animalear.js

--- Priyansh/commands/animalear.js
+++ Priyansh/commands/animalear.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "ear",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "uzairrajput",
 	description: "Animal Ear Waifu",
 	commandCategory: "nsfw",
 	usages: "ear",
