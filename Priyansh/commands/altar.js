3458871cad0cc45c78f557d01bbf0294792bd5b0
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-10 00:15:50 +0500

    Create altar.js

--- Priyansh/commands/altar.js
+++ Priyansh/commands/altar.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "altar",
 	version: "1.0.2",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "uzairrajput",
 	description: "altar [mentioned]",
 	commandCategory: "edit-image",
 	usages: "[@tag]",
