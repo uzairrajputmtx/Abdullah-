04c638a387cc78af7661a2c80d0f94a7e8a1187f
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-10 00:24:42 +0500

    Create animegirl.js

--- Priyansh/commands/animegirl.js
+++ Priyansh/commands/animegirl.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "animegirl",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "uzairrajput",
   description: "Random Loli Pics",
   commandCategory: "nsfw",
   usages: "lolilewd",
