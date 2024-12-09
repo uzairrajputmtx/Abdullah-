9e2453ee574fe12cfe5c759673d275162d8dbfe7
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-10 00:32:12 +0500

    Create antiout.js

--- Priyansh/commands/antiout.js
+++ Priyansh/commands/antiout.js
@@ -1,7 +1,7 @@
 module.exports.config = {
     name: "antiout",
     version: "1.0.0",
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "uzairrajput",
     hasPermssion: 1,
     description: "Turn off antiout",
     usages: "antiout on/off",
