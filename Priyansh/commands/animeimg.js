8acb40e3e09b9dd89231fc8eb3219edd6bc826b1
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-10 00:27:46 +0500

    Update animeimg.js

--- Priyansh/commands/animeimg.js
+++ Priyansh/commands/animeimg.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "imganime",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "uzairrajput",
 	description: "Random ảnh anime",
 	commandCategory: "Random-IMG",
 	usages: "imganime",
