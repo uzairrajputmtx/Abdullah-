f9b0deaa1ad142313120d22de5a888b0a6cf6f32
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-09 20:19:45 +0500

    Update anifact.js

--- Priyansh/commands/anifact.js
+++ Priyansh/commands/anifact.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "anifact",
     version: "1.0.0",
     hasPermision: 0,
-    credit: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credit: "uzairrajput",
     description: "random animal fact by Juswa",
     commandCategory: "random-img",
     cooldowns: 0,
