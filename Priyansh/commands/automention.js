85000178cb063e14ace3c702a528324d269da908
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-10 21:23:29 +0500

    Create automention.js

--- Priyansh/commands/automention.js
+++ Priyansh/commands/automention.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "automention",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "uzairrajput",
 	description: "automent [mentioned]",
 	commandCategory: "other",
 	cooldowns: 5
