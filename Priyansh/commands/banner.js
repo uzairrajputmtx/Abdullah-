c4869f68ceb8200694b7c28771b8c4c3e3fddf6d
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-10 22:08:28 +0500

    Create banner.js

--- Priyansh/commands/banner.js
+++ Priyansh/commands/banner.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "banner",
 	version: "1.0.2",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "uzairrajput",
 	description: "generates banner with lots of characters available",
   commandCategory: "game",
 	usages: "{number}|{name1}|{name2}|{name3}|{color}",
