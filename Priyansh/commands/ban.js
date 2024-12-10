fc72ec4d11b4f61296b90f115c4eefbb792578ff
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-10 22:05:20 +0500

    Create ban.js

--- Priyansh/commands/ban.js
+++ Priyansh/commands/ban.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "ban",
 	version: "2.0.5",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "uzairrajput",
 	description: "Permanently ban members from the group (Remember to set the qtv bot)\nAuthor: 𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
 	commandCategory: "group",
 	usages: "[key]",
