555354e70fb3ff623c0bbb16f2af887c59e52ea2
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-12 13:37:38 +0500

    Create baybayin.js

--- Priyansh/commands/baybayin.js
+++ Priyansh/commands/baybayin.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "baybayin",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "uzairrajput",
 	description: "convert text to baybayin",
 	commandCategory: "Phương tiện",
 	usages: "baybayin [text]",
