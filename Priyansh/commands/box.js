cce8ce4af3ccfbe69d461b8803e444e74384bef2
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-12 13:57:36 +0500

    Create box.js

--- Priyansh/commands/box.js
+++ Priyansh/commands/box.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "group",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "uzairrajput",
     description: "Parent group settingst.",
 	commandCategory: "box",
 	usages: "[name/emoji/admin/image/info]",
