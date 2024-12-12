310ddc1c626852180ac61d4a8364c54e08c16d74
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-12 13:41:10 +0500

    Create bestie.js

--- Priyansh/commands/bestie.js
+++ Priyansh/commands/bestie.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "bestie",
   version: "7.3.1",
   hasPermssion: 0,
-  credits: "AADI BABU", 
+  credits: "uzairrajput", 
   description: "Get Pair From Mention",
   commandCategory: "png",
   usages: "[@mention]",
