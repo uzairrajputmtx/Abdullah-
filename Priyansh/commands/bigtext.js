8d5dfbd07161a51e3774bbaf438cdcae21daf317
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-12 13:43:30 +0500

    Create bigtext.js

--- Priyansh/commands/bigtext.js
+++ Priyansh/commands/bigtext.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "bigtext",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "uzairrajput",
     description: "big text",
     commandCategory: "game",
     usages: "bigtext <text>",
