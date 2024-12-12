4395606feb5918389060301ddc6d1e168f11f031
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-12 13:35:43 +0500

    Create batmanslap.js

--- Priyansh/commands/batmanslap.js
+++ Priyansh/commands/batmanslap.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "batslap",
     version: "2.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "uzairrajput",
     description: "",
     commandCategory: "general",
     usages: "[tag]",
