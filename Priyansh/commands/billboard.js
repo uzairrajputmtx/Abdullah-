d6dd85937c60579a33f50aa0ad483a886c0996e4
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-12 13:45:59 +0500

    Create billboard.js

--- Priyansh/commands/billboard.js
+++ Priyansh/commands/billboard.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "billboard",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "uzairrajput",
 	description: "Comment trên pỏnhub ( ͡° ͜ʖ ͡°)",
 	commandCategory: "edit-img",
 	usages: "billoard [text]",
